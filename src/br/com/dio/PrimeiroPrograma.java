1f37e2340cb355f9564e6be5851879e9c388820e
Luiza <luiza_dealmeida@hotmail.com>
2022-07-05 18:13:34 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,21 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		Livro livro = new Livro();
+		
+		System.out.println(gato);
+        System.out.print(livro);
+	}
+
+}
+
+	class Livro {
+		private String nome;
+		private String npag;
+	}
